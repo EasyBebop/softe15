f857e4bd4b7ba64f904a906af7764596c73d16cf
nabivins <nabivins@email.uark.edu>
2015-03-05 14:02:17 -0600

    Answer Form
    Added base form to be used for user answer selection.

---
+++ SEGroup15/public_html/game.php
@@ -0,0 +1,29 @@
+<!DOCTYPE HTML>
+<html>
+    
+    <head>
+        <title>WIP</title>
+    </head>
+    <body>
+        <h1>Future location for single player</h1>
+        <?php
+            $score = 0;
+            $question = NULL;
+            $correct = NULL;
+            $false1 = NULL;
+            $false2 = NULL;
+            $false3 = NULL;
+        {
+        ?>
+        <form id="answerForm" method="post">
+	<input type="text" id="a1">
+	<input type="text" id="a2">
+	<input type="text" id="a3">
+        <input type="text" id="a4">
+	</form>
+        <?php
+        }
+        ?>
+    </body>
+    
+</html>
