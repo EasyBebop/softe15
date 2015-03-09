a7a9349550fc4e07e6e29f7fa59a274861a21d27
JohanHoyt <chenguille@hotmail.com>
2015-03-08 23:05:29 -0500

    new layout for account creation
    layout

--- SEGroup15/public_html/createAccount.php
+++ SEGroup15/public_html/createAccount.php
@@ -6,27 +6,29 @@ and open the template in the editor.
 -->
 <html>
     <head>
-        
+        <img style="position:absolute; left:0; right:0; top:-100px; margin:auto;" src="logo.png" alt="T.R.I.V.I.A" width="300" height="300">
         <style>
-            html{ background-color : gray ;}
-            body{ margin: 50px; background-color: white; height:500px;}
+            html{ background-color:#CCE6FF;}
+            body{ margin-left:250px; margin-right:250px;margin-top:130px; margin-bottom:130px; background-color: white; height:900px;}
             p{text-align: center; font-size:25px;}
             #form{margin-left:20px; font-size:20px;}
             
-            
         </style>
+   
         
         <meta charset="UTF-8">
         <title>T.R.I.V.I.A log in </title>
     </head>
     <body>
+        
         <p> <br>Create a new account </p>
         <div id="form">
             <form method="post" action="" name="logForm" id="logForm"><br>
                  Username: <input type="text" name="username" id="username"><br>
                 Password: <input type="text" name="password" id="password"><br>
                 Confirm Password: <input type="text" name="confirmPassword" id="confirmPassword"><br><br>
-                <input type="submit" value="Submit" form_id="cform"><br><br>
+                <img style="position:relative; left:20px;" src="back.png" alt="back" width="60" height="60">
+                <img style="position:relative; left:200px;" src="submit.png" alt="submit" width="60" height="60">
             </form>
         </div>
             
