5b4afe685acb043159a1fc7f3f4432ca46cc44c4
JohanHoyt <chenguille@hotmail.com>
2015-03-11 21:34:07 -0500

    questions won't repeat, added score

--- SEGroup15/public_html/game.php
+++ SEGroup15/public_html/game.php
@@ -18,6 +18,16 @@
     $selected = mysql_select_db("questions",$dbhandle) 
     or die("Could not select examples");
     
+    //keep score
+    if(isset($_POST['score']))
+    {
+        $score = $_POST[score];
+    }
+    else
+    {
+        $score = 0; 
+    }
+    
     //get number of questions available
     $result1 = mysql_query("select count(1) FROM questions");
     $numRow = mysql_fetch_array($result1);
@@ -30,11 +40,8 @@
     $result2 = mysql_query($sql);
     
         $row = mysql_fetch_array($result2) or die(mysql_error());  
-//        $option1 = $row['correct'];
-//        $option2 = $row['fake1'];
-//        $option3 = $row['fake2'];
-//        $option4 = $row['fake3'];
         $question = $row['Q'];
+        $questionID = $row['QID'];
         $options = array("$row[correct]","$row[fake1]","$row[fake2]","$row[fake3]");
         shuffle($options);
     
@@ -49,6 +56,25 @@
             .option { text-decoration:none; color: #204081; width: 500px; text-align: center; border: 1px solid #9325BC; padding: 10px; position:absolute; left:0; right:0; margin:auto; font-size:25px; } 
             .option:hover { -moz-box-shadow: 0 0 20px #ccc; -webkit-box-shadow: 0 0 20px #ccc; box-shadow: 0 0 10px #ccc; }
          </style>
+         <?php
+            //check past answer
+            if(isset($_POST['answer']))
+            {
+                $sql2 = "SELECT * FROM questions WHERE QID = $_POST[pastQ]";
+                $query1 = mysql_query($sql2);
+                $correct = mysql_fetch_array($query1);
+                if($_POST[answer] == $correct[correct])
+                {
+                    echo "<script type='text/javascript'>alert(\"GOOD! Correct answer\");</script>";            
+                    $score = $score + 10;
+                    
+                }
+                else
+                {
+                    echo "<script type='text/javascript'>alert(\"TOO BAD! that's not correct!\");</script>";
+                }
+            }
+        ?>
     </head>
     <body>
         <img style="position:absolute; left:0; right:0; top:130px;  margin:auto;" src="navbar.png" alt="navbar" width="900" height="40">
@@ -60,10 +86,15 @@
         
         <!-- options -->  
         <?php
+        echo "<br> Your score: $score<br>";
         for( $i = 0; $i < sizeof($options); $i++)
         {
              echo "<div> 
+                
             <form method=\"post\" action=\"\" name=\"option$i\" id=\"option$i\">
+                <input type=\"hidden\" name=\"answer\" id=\"answer\" value=\"$options[$i]\">
+                <input type=\"hidden\" name=\"pastQ\" id=\"pastQ\" value=\"$questionID\">
+                <input type=\"hidden\" name=\"score\" id=\"score\" value=\"$score\">    
                 <br><br><br>
                 <a class=\"option\" href=\"#\" onclick=\"document.option$i.submit();\">$options[$i]</a> 
             </form>
