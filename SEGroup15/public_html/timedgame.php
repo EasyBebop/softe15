41db4e91e5cb6ccec869f5fef9ba5731e210570d
nabivins <nabivins@email.uark.edu>
2015-03-31 22:35:18 -0500

    More Changes
    yeah

--- SEGroup15/public_html/timedgame.php
+++ SEGroup15/public_html/timedgame.php
@@ -33,7 +33,8 @@
     else {
         $questionsanswered = 0;
     }
-    
+    if($questionsanswered == 10)
+    { 
     //get number of questions available
     $result1 = mysql_query("select count(1) FROM questions");
     $numRow = mysql_fetch_array($result1);
@@ -52,7 +53,7 @@
     ?>
         
     <!--layout-->    
-    <title>Playing a game</title>
+    <title>Time Attack!!</title>
     <a href="index.php"><img style="position:absolute; left:0; right:0; top:-30px; margin:auto;" src="logo.png" alt="T.R.I.V.I.A" width="300" height="200"></a>
         <style>
             html{ background-color:#CCE6FF;}
@@ -107,6 +108,22 @@
             </form>
             </div> <br><br>";
         }
+    }
+        else
+        {
         ?>
+        <title>Time Attack!!</title>
+        <a href="index.php"><img style="position:absolute; left:0; right:0; top:-30px; margin:auto;" src="logo.png" alt="T.R.I.V.I.A" width="300" height="200"></a>
+        <style>
+         html{ background-color:#CCE6FF;}
+         body{ margin-left:250px; margin-right:250px;margin-top:150px; margin-bottom:130px; background-color: white; height:900px;}
+         p{text-align: center; font-size:25px;}
+         .option { text-decoration:none; color: #204081; width: 500px; text-align: center; border: 1px solid #9325BC; padding: 10px; position:absolute; left:0; right:0; margin:auto; font-size:25px; } 
+         .option:hover { -moz-box-shadow: 0 0 20px #ccc; -webkit-box-shadow: 0 0 20px #ccc; box-shadow: 0 0 10px #ccc; }
+        </style>
+        <?php
+            
+        }
+    ?>
     </body>  
 </html>
